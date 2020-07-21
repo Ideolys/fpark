f31304000f17039b8f07324cdc5cbfb391f3fb66
Matthieu Robin <mrobin@ideolys.com>
2020-07-21 14:38:00 +0200

    Add tests for GET

--- tests/test.get.js
+++ tests/test.get.js
@@ -1,7 +1,9 @@
 const server  = require('../src/index');
 const utils   = require('./utils');
 const request = require('../src/commons/request');
-const node    = require('./datasets/configs/100.json').NODES[0];
+const nodes   = require('./datasets/configs/100.json').NODES;
+const fs      = require('fs');
+const path    = require('path');
 
 describe.only('API GET', () => {
 
@@ -17,7 +19,7 @@ describe.only('API GET', () => {
 
     it('should return a 404 if the file does not exist', done => {
       request({
-        base : node.host,
+        base : nodes[1].host,
         path : '/file/container/test/1.jpg',
       }, (err, res) => {
         should(err).ok();
@@ -28,7 +30,7 @@ describe.only('API GET', () => {
 
     it('should get a file with gzip', done => {
       request({
-        base : node.host,
+        base : nodes[1].host,
         path : '/file/container/test/image.jpg',
       }, (err, res) => {
         should(err).not.ok();
@@ -40,7 +42,7 @@ describe.only('API GET', () => {
 
     it('should set cache-control header', done => {
       request({
-        base : node.host,
+        base : nodes[1].host,
         path : '/file/container/test/image.jpg',
       }, (err, res) => {
         should(err).not.ok();
@@ -52,14 +54,54 @@ describe.only('API GET', () => {
 
     it('should set cache-control header', done => {
       request({
-        base : node.host,
+        base : nodes[1].host,
         path : '/file/container/test/image.jpg',
       }, (err, res) => {
         should(err).not.ok();
         should(res.statusCode).eql(200);
         should(res.headers['cache-control']).be.a.String().and.eql('max-age=7776000,immutable');
-        utils.stopExecute(done);
+        done();
       });
+    });
+
+    describe('Inter-node communication', () => {
+
+      it('should get a file not present on current node 101 : [File] 100; 200; 101', done => {
+        request({
+          base : nodes[0].host,
+          path : '/file/container/test/image.jpg',
+        }, (err, res) => {
+          should(err).not.ok();
+          should(res.statusCode).eql(200);
+
+          let pathDir = path.join(__dirname, 'datasets', 'get', 'data_100', '100-200-101');
+          fs.access(path.join(pathDir, 'test', 'image.jpg.enc'), (err) => {
+            should(err).not.ok();
+
+            utils.deleteFolderRecursive(pathDir);
+            done();
+          });
+        });
+      });
+
+      it('should get a file not present on current node 200 : [File] 100; 200; 101', done => {
+        request({
+          base : nodes[2].host,
+          path : '/file/container/test/image.jpg',
+        }, (err, res) => {
+          should(err).not.ok();
+          should(res.statusCode).eql(200);
+
+          let pathDir = path.join(__dirname, 'datasets', 'get', 'data_200', '100-200-101');
+          fs.access(path.join(pathDir, 'test', 'image.jpg.enc'), (err) => {
+            should(err).not.ok();
+
+            utils.deleteFolderRecursive(pathDir);
+            done();
+          });
+        });
+      });
+
     });
 
   });
