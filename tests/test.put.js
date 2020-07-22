e589fb2efe5b054a880eb3209d25c7070db11d90
Matthieu Robin <mrobin@ideolys.com>
2020-07-22 17:37:09 +0200

    Add test for encryption in PUT

--- tests/test.put.js
+++ tests/test.put.js
@@ -5,6 +5,8 @@ const utils    = require('./utils');
 const config   = require('./datasets/configs/100.json');
 const nodes    = config.NODES;
 const FormData = require('form-data');
+const crypto   = require('crypto');
+const file     = require('../src/commons/file');
 
 describe('API PUT', () => {
 
@@ -66,6 +68,59 @@ describe('API PUT', () => {
             pathDir  = path.join(__dirname, 'datasets', 'put', 'data_101', '101-200-201');
             fs.access(path.join(pathDir, 'test', filename + '.enc'), fs.constants.F_OK, err => {
               should(err).not.ok();
+              utils.deleteFolderRecursive(pathDir);
+              done();
+            });
+          });
+        }).catch(err => {
+          done(err);
+        });
+      });
+
+      it('should upload a file & encrypt', done => {
+        let formData = new FormData();
+        formData.append('file', fs.createReadStream(path.join(__dirname, 'datasets', '_documents', 'file.txt')));
+
+        fetch(nodes[1].host + '/file/container/test/file.txt', {
+          method  : 'PUT',
+          body    : formData
+        }).then(res => {
+          should(res.status).eql(200);
+
+          let pathDir  = path.join(__dirname, 'datasets', 'put', 'data_101', '101-200-201');
+          let filename = utils.getFileHash('file.txt', config.HASH_SECRET);
+          let filePath = path.join(pathDir, 'test', filename + '.enc');
+          fs.access(filePath, fs.constants.F_OK, err => {
+            should(err).not.ok();
+
+            let filenameEncrypt = file.getFileName('file.txt', config.ENCRYPTION_IV_LENGTH);
+            let cipher          = crypto.createDecipheriv(config.ENCRYPTION_ALGORITHM, filenameEncrypt, config.ENCRYPTION_IV);
+            let fileData        = fs.readFileSync(filePath);
+
+            should(fileData).not.eql('test\n');
+
+            let decrypted = cipher.update(fileData);
+            decrypted    += cipher.final();
+
+            should(decrypted).eql('test\n');
+            utils.deleteFolderRecursive(pathDir);
+
+            pathDir  = path.join(__dirname, 'datasets', 'put', 'data_200', '101-200-201');
+            filePath = path.join(pathDir, 'test', filename + '.enc');
+            fs.access(filePath, fs.constants.F_OK, err => {
+              should(err).not.ok();
+
+              filenameEncrypt = file.getFileName('file.txt', config.ENCRYPTION_IV_LENGTH);
+              cipher          = crypto.createDecipheriv(config.ENCRYPTION_ALGORITHM, filenameEncrypt, config.ENCRYPTION_IV);
+              fileData        = fs.readFileSync(filePath);
+
+              should(fileData).not.eql('test\n');
+
+              decrypted = cipher.update(fileData);
+              decrypted += cipher.final();
+
+              should(decrypted).eql('test\n');
+
               utils.deleteFolderRecursive(pathDir);
               done();
             });
